436ef6b1fd4f407a0c0aeda6a9bd06760fd5e444
jmguru@gmail.com <jmguru@gmail.com>
2017-02-01 00:07:08 -0600

    Updated Rev1

--- test1/README.md
+++ test1/README.md
@@ -3,3 +3,12 @@ Rev 1 <br>
 --Read JSON file <br>
 --Load into memory <br>
 --Print variables <br>
+
+Rev 2 <br>
+--Create web interface
+-- 1. Three forms: text, text, button, text
+-- ex> first, service, submit, result
+-- 2. Callback
+-- ex> On submit, pass the firstname, and service into the app
+-- ey> pass parameters into function
+-- ez> send back username/password to screen.
--- test1/app.js
+++ test1/app.js
@@ -1,3 +1,32 @@
 
-console.log("Hello fucker.");
+var fs = require("fs");
 
+var contents = fs.readFileSync("config/registry.json");
+var jsonContent = JSON.parse(contents);
+var userobj = {
+                  username: "",
+                  password: ""
+                }
+
+getUsernamePassword(jsonContent,"Terri","Paypal",userobj);
+
+console.log(userobj.username);
+console.log(userobj.password);
+
+function getUsernamePassword(json,client,servicename,payload) {
+  var num = Object.keys(json.Person).length;
+
+  for ( var i = 0; i < num; i++)
+  {
+      if (client == json.Person[i].firstname) {
+          var obj = json.Person[i].services;
+          var num2 = Object.keys(obj).length;
+          for (var j = 0; j < num2; j++) {
+            if(servicename == obj[j].name) {
+              payload.username = obj[j].username;
+              payload.password = obj[j].password;
+            }
+          }
+      }
+  }
+}
